c068b05c900e281b8b29b321fde14df292133c72
Ankit Singh <ankit@Ankits-MacBook-Pro-2.local>
2016-08-24 13:05:02 +0530

    wwar working

--- src/main/java/com/practo/wp/run/Application.java
+++ src/main/java/com/practo/wp/run/Application.java
@@ -3,20 +3,24 @@ package com.practo.wp.run;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.stereotype.Controller;
 
-@SpringBootApplication
+
 @ImportResource("classpath:hibernate.xml")
 @ComponentScan(basePackages = {"com.practo.wp"})
 @EntityScan("com.practo.wp.data.entity")
 @EnableJpaRepositories("com.practo.wp.data.dao")
-@Controller
+@SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(Application.class);
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
